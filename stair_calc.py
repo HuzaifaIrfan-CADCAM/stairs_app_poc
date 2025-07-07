bceb719f4cf48ec064e0ae07c018735e5229f98f
HuzaifaIrfan <huzaifairfan2001@gmail.com>
2025-07-07 20:16:33 +0500

    calc for cut list

--- stair_calc.py
+++ stair_calc.py
@@ -9,13 +9,15 @@ def main():
     print("Hello from stair-manufacturing!")
 
     result = auto_stair_calculator(
-        total_rise=120,
-        step_height=7.5,
+        total_rise=122,
+        step_height=7.625,
         tread_thickness=1.0,
-        tread_depth=10.0,
+        tread_depth=11.5,
         mount_type="standard"
     )
+    result["width_stair"] =36.75
     pprint(result)
+
 
 if __name__ == "__main__":
     main()
